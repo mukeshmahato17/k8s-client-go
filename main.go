c859eee874c0428c6e6d1972d5fa577eb0dd2eeb
mukeshmahato17 <mukeshmahato1089@gmail.com>
2024-07-05 13:38:39 +0545

    handling errors

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"log"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -15,18 +14,18 @@ func main() {
 	kubeconfig := flag.String("kubeconfig", clientcmd.RecommendedHomeFile, "location to your kubeconfig file")
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("error %s building config from flags", err.Error())
 	}
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		panic(err)
+		fmt.Printf("error %s, creating clientset", err.Error())
 	}
 
 	ctx := context.Background()
 
 	pods, err := clientset.CoreV1().Pods("default").List(ctx, metav1.ListOptions{})
 	if err != nil {
-		panic(err)
+		fmt.Printf("error %s, while listing all the pods from default namespace", err.Error())
 	}
 	fmt.Println("Pods from default namespace")
 	for _, pod := range pods.Items {
@@ -35,7 +34,7 @@ func main() {
 
 	deployments, err := clientset.AppsV1().Deployments("default").List(ctx, metav1.ListOptions{})
 	if err != nil {
-		panic(err)
+		fmt.Printf("error %s, while listing deployments from default namespace", err.Error())
 	}
 	fmt.Println("Deployments from default namespace")
 	for _, deploy := range deployments.Items {
